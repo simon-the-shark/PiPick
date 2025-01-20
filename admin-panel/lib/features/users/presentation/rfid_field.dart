94b34d0b98f2fb819495fbe8ff5863352af94540
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-01-20 18:35:53 +0100

    feat(rfid): integrate zone selection and loading dialog for RFID scanning

--- admin-panel/lib/features/users/presentation/rfid_field.dart
+++ admin-panel/lib/features/users/presentation/rfid_field.dart
@@ -1,25 +1,131 @@
+import "dart:async";
+
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:reactive_forms_annotations/reactive_forms_annotations.dart";
 import "package:toastification/toastification.dart";
 
+import "../../../database/models.dart";
 import "../../../mqtt_client.dart";
+
+Future<AccessZone?> showZoneSelectionDialog(
+  BuildContext context,
+  IList<AccessZone> zones,
+) {
+  return showDialog<AccessZone>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Wybierz strefę"),
+        content: SizedBox(
+          width: double.maxFinite,
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: zones.length,
+            itemBuilder: (context, index) {
+              final zone = zones[index];
+              return ListTile(
+                title: Text("Strefa ${zone.number}"),
+                subtitle: Text(zone.location),
+                onTap: () {
+                  Navigator.of(context).pop(zone);
+                },
+              );
+            },
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text("Anuluj"),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<void> showLoadingDialog(
+  BuildContext context,
+  WidgetRef ref,
+  int zoneId,
+  FormControl<String>? rfidCardControl,
+) {
+  StreamSubscription<AccessMessage>? stream;
+
+  stream = ref.watch(mqttClientProvider).value?.listen((event) async {
+    print(event);
+    if (event.zoneId == zoneId) {
+      await stream?.cancel();
+      Navigator.of(context).pop();
+
+      rfidCardControl?.value = event.rfidCard;
+
+      toastification.show(
+        context: context,
+        title: const Text("Karta zeskanowana"),
+        description: const Text("Użytkownik ma dostęp do strefy"),
+        type: ToastificationType.success,
+        autoCloseDuration: const Duration(seconds: 2),
+      );
+    }
+  });
+
+  return showDialog(
+    context: context,
+    barrierDismissible: false, // Prevent dismissing by tapping outside
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: const SizedBox(
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              SizedBox(height: 20),
+              Text("Waiting for card scan..."),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              // Cancel the MQTT listener
+              await stream?.cancel();
+
+              // ref.read(singleLogProvider.notifier).cancel();
+              if (context.mounted) {
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text("Cancel"),
+          ),
+        ],
+      );
+    },
+  );
+}
 
 class RfidField extends ConsumerWidget {
   const RfidField({
     super.key,
     required this.rfidCardControl,
+    required this.zones, // Add this parameter
   });
+
   final FormControl<String>? rfidCardControl;
+  final IList<AccessZone> zones;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final _ = ref.watch(mqttClientProvider);
-
     return ReactiveTextField<String>(
       readOnly: true,
       onTap: (_) {
         toastification.show(
+          context: context,
           title: const Text("Kartę RFID można tylko zeskanować"),
           type: ToastificationType.warning,
           autoCloseDuration: const Duration(seconds: 2),
@@ -40,8 +146,18 @@ class RfidField extends ConsumerWidget {
             Tooltip(
               message: "Zeskanuj kartę RFID",
               child: IconButton(
-                onPressed: () {
-                  rfidCardControl?.value = "RANDOM-RFID-CARD-NUMBEr";
+                onPressed: () async {
+                  final selectedZone =
+                      await showZoneSelectionDialog(context, zones);
+                  print(selectedZone);
+                  if (selectedZone != null) {
+                    await showLoadingDialog(
+                      context,
+                      ref,
+                      selectedZone.id,
+                      rfidCardControl,
+                    );
+                  }
                 },
                 icon: const Icon(Icons.spatial_tracking_sharp),
               ),
